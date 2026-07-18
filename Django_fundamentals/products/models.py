5c23d35bae09110507d96838878a08cfc59072c6
Mamun Bepari <beparimamun708@gmail.com>
2026-07-18 23:10:13 +0600

    completed urls.py

--- Django_fundamentals/products/models.py
+++ Django_fundamentals/products/models.py
@@ -4,13 +4,22 @@ class Product(models.Model):
 
    name = models.CharField(max_length=255)
 
-   price = models.DecimalField(max_digits=10, decimal_places=2)
+   price = models.DecimalField(
+      max_digits=10, 
+      decimal_places=2
+   )
 
    stock = models.PositiveIntegerField()
 
-   created_at = models.DateTimeField(auto_now_add=True)
+   created_at = models.DateTimeField(
+      auto_now_add=True
+   )
 
-   updated_at = models.DateTimeField(auto_now=True)
+   updated_at = models.DateTimeField(
+      auto_now=True
+   )
 
+   def __str__(self):
+      return self.name 
 
 
--- Django_fundamentals/products/views.py
+++ Django_fundamentals/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def product_list(request):
+   return HttpResponse("Products Page Open.")
+
+def home(request):
+   return HttpResponse("Home Page Open.")
+
+def about(request):
+   return HttpResponse("About Page Open.")
+
+def contact(request):
+   return HttpResponse("Contact Page Open.")
+
+
+
