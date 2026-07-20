8a8dadb678c741ce59e7b2c4fc580d09f51bee4f
Mamun Bepari <beparimamun708@gmail.com>
2026-07-20 20:58:08 +0600

    Added admin panel and register

--- Django_fundamentals/students/admin.py
+++ Django_fundamentals/students/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+
+   list_display = (
+      "id",
+      "name",
+      "email",
+      "age",
+      "created_at",
+   )
+
+   search_fields = (
+      "name",
+      "email",
+   )
+
+   list_filter = (
+      "age",
+      "created_at",
+   )
+
+   ordering = (
+      "name",
+      # "-name",
+      # "age",
+      # "-age",
+   )
+
+   list_per_page = 10
