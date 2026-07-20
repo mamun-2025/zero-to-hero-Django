3c19a41d459dd460a56dacd72608125a8d35da60
Mamun Bepari <beparimamun708@gmail.com>
2026-07-20 20:23:21 +0600

    Added database model design

--- Django_fundamentals/students/models.py
+++ Django_fundamentals/students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+
+   name = models.CharField(max_length=255)
+
+   email = models.EmailField(unique=True)
+
+   age = models.PositiveIntegerField()
+
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+      return self.name 
+   
+
