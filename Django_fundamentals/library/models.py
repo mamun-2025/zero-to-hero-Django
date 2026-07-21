eaae094993ee3e81d9f5020823a7ceed8d5cb413
Mamun Bepari <beparimamun708@gmail.com>
2026-07-21 20:46:26 +0600

    Added author table and book table

--- Django_fundamentals/library/models.py
+++ Django_fundamentals/library/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+# Author Model
+class Author(models.Model):
+
+   name = models.CharField(max_length=100)
+
+   bio = models.TextField(blank=True)
+
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+      return self.name 
+   
+
+# Book Model
+class Book(models.Model):
+
+   title = models.CharField(max_length=255)
+
+   price = models.DecimalField(
+      max_digits=8, 
+      decimal_places=2,
+   )
+
+   published_date = models.DateField()
+
+   author = models.ForeignKey(
+      Author,
+      on_delete=models.CASCADE,
+
+   )
+
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+      return self.title
