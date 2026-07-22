0feade765455ddd40ebbf2f8c7c61a16eb852a90
Mamun Bepari <beparimamun708@gmail.com>
2026-07-22 23:26:10 +0600

    Added model

--- Django_fundamentals/employees/models.py
+++ Django_fundamentals/employees/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+   name = models.CharField(max_length=100)
+
+   email = models.EmailField(unique=True)
+
+   salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+   department = models.CharField(max_length=100)
+
+   joining_date = models.DateField()
+
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+      return self.name 
