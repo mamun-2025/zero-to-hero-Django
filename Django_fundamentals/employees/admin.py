84824df08cf79fb66c2ac1cbab20484f23f02d35
Mamun Bepari <beparimamun708@gmail.com>
2026-07-23 00:40:51 +0600

    Added admin panel

--- Django_fundamentals/employees/admin.py
+++ Django_fundamentals/employees/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+
+   list_display = (
+      "id",
+      "name",
+      "email",
+      "department",
+      "salary",
+      "joining_date",
+   )
+
+   list_filter = (
+      "department",
+      "joining_date",
+   )
+
+   search_fields = (
+      "name",
+      "email",
+   )
+
+   ordering = (
+      "name",
+   )
+
+   list_per_page = 20
+
+   readonly_fields = (
+      "created_at",
+      "updated_at",
+   )
+
