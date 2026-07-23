d16ca96e2e868edfaf3b039f9dfd606898368d8e
Mamun Bepari <beparimamun708@gmail.com>
2026-07-24 00:53:30 +0600

    Added widgets and clean() method

--- Django_fundamentals/employees/forms.py
+++ Django_fundamentals/employees/forms.py
@@ -20,50 +20,191 @@
 #    joinin_date = forms.DateField
 
 
-from django import forms 
+
+#### Most Important 
+# from django import forms 
+# from .models import Employee
+# from django.core.exceptions import ValidationError
+
+# class EmployeeModelForm(forms.ModelForm):
+
+#    class Meta:
+      
+#       model = Employee
+
+#       fields = [
+#          "name",
+#          "email",
+#          "salary",
+#          "department",
+#          "joining_date",
+#       ]
+
+#       widgets = {
+
+#          "name": forms.TextInput(
+#             attrs={
+#                "class": "form-control",
+#                "placeholder": "Enter employee name",
+#                "required": True,
+#             }
+#          ),
+
+#          "email": forms.EmailInput(
+#             attrs={
+#                "class": "form-control",
+#                "placeholder": "Enter email",
+#                "required": True,
+#             }
+#          ),
+
+#          "salary": forms.NumberInput(
+#             attrs={
+#                "class": "form-control",
+#                "placeholder": "Enter salary",
+#                "min": 0,
+#                "step": "0.01",
+#             }
+#          ),
+
+#          "department": forms.TextInput(
+#             attrs={
+#                "class": "form-control",
+#                "placeholder": "Enter Department",
+#             }
+#          ),
+
+#          "joining_date": forms.DateInput(
+#             attrs={
+#                "class": "form-control",
+#                "type": "date",
+#             }
+#          ),
+#       }
+
+
+
+#    def clean_email(self):
+
+#       email = self.cleaned_data["email"]
+
+#       if Employee.objects.filter(email=email).exists():
+
+#          raise ValidationError(
+#             "This email already exists."
+#          )
+
+#       return email
+
+
+
+#    def clean_salary(self):
+
+#       salary = self.cleaned_data["salary"]
+
+#       if salary <= 0:
+#          raise ValidationError(
+#             "Salary must be greater than zero."
+#          )
+
+#       if salary < 10000:
+#          raise ValidationError(
+#             "Minimum salary is 10000"
+#          )
+
+#       return salary
+
+
+
+from django import forms
 from .models import Employee
 from django.core.exceptions import ValidationError
+from datetime import date 
 
 class EmployeeModelForm(forms.ModelForm):
-
+    
    class Meta:
-      
+        
       model = Employee
+      fields = "__all__"
 
-      fields = [
-         "name",
-         "email",
-         "salary",
-         "department",
-         "joining_date",
-      ]
 
-   def clean_email(self):
+      widgets = {
 
-      email = self.cleaned_data["email"]
+         "name": forms.TextInput(
+            attrs={
+               "class": "form-control",
+               "placeholder": "Enter employee name",
+               "required": True,
+            }
+         ),
 
-      if Employee.objects.filter(email=email).exists():
+         "email": forms.EmailInput(
+            attrs={
+               "class": "form-control",
+               "placeholder": "Enter email",
+               "required": True,
+            }
+         ),
 
-         raise ValidationError(
-            "This email already exists."
+         "department": forms.TextInput(
+            attrs={
+               "class": "form-control",
+               "placeholder": "Enter department",
+            }
+         ),
+
+         "salary": forms.NumberInput(
+            attrs={
+               "class": "form-control",
+               "placeholder": "Enter salary",
+               "min": 0,
+               "step": "0.01",
+            }
+         ),
+
+         "joining_date": forms.DateInput(
+            attrs={
+               "class": "form-control",
+               "type": "date",
+            }
+         ),
+      }
+
+
+
+   # Multiple Field Validation
+   def clean(self):
+
+      cleaned_data = super().clean()
+
+      department = cleaned_data.get("department")
+
+      salary = cleaned_data.get("salary")
+
+      joining_date = cleaned_data.get("joining_date")
+
+      if (department == "HR" and salary < 20000):
+
+         self.add_error(
+            "salary",
+            "HR salary must be at least 20000."
          )
 
-      return email
+      if (department == "IT" and salary < 30000):
 
-
-
-   def clean_salary(self):
-
-      salary = self.cleaned_data["salary"]
-
-      if salary <= 0:
          raise ValidationError(
-            "Salary must be greater than zero."
+            "IT salary must be at least 30000."
          )
 
-      if salary < 10000:
+      if (department == "Finance" and joining_date > date.today()):
+
          raise ValidationError(
-            "Minimum salary is 10000"
+            "Joinig date cannot be in the future."
          )
 
-      return salary
+      return cleaned_data
+
+   
+
+
