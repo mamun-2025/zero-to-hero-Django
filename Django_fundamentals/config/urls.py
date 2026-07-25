e2a30af0c4c78a441105075ad49032ae7dbbafed
Mamun Bepari <beparimamun708@gmail.com>
2026-07-25 11:31:15 +0600

    Completed blog app

--- Django_fundamentals/config/urls.py
+++ Django_fundamentals/config/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import include, path 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,6 +12,14 @@ urlpatterns = [
     path("employees/", include("employees.urls")),
     path("blogs/", include("blog.urls")),
 ]
+
+if settings.DEBUG:
+   urlpatterns += static(
+      settings.MEDIA_URL,
+      document_root=settings.MEDIA_ROOT,
+   )
+
+
 
 
 
