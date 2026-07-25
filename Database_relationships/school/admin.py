4989b6599541bc01b32721e64093a50dd0061a99
Mamun Bepari <beparimamun708@gmail.com>
2026-07-25 14:06:17 +0600

    Create model, admin, urls

--- Database_relationships/school/admin.py
+++ Database_relationships/school/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Teacher
 
-# Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+
+   list_display = (
+      "id",
+      "name",
+      "email",
+   )
+
+   search_fields = (
+      "name",
+      "email",
+   )
+
+   ordering = (
+      "name",
+   )
+
+   readonly_fields = (
+      "created_at",
+      "updated_at",
+   )
--- Database_relationships/school/models.py
+++ Database_relationships/school/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+   name = models.CharField(max_length=100)
+   email = models.EmailField(unique=True)
+   department = models.CharField(max_length=100)
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+      return self.name 
+
+   class Meta:
+      ordering = ["name"]
+      verbose_name = "Teacher"
+      verbose_name_plural = "Teachers"
