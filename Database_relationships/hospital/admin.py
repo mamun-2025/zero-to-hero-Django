e7e134e4e2742a80b5b182381e8cc4143be1da8a
Mamun Bepari <beparimamun708@gmail.com>
2026-07-26 09:54:15 +0600

    add model and admin

--- Database_relationships/hospital/admin.py
+++ Database_relationships/hospital/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from .models import Doctor, Patient, Appointment
 
-# Register your models here.
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+   list_display = (
+      "id",
+      "name",
+      "specialization",
+   )
+
+   search_fields = (
+      "name",
+      "specialization",
+   )
+
+   list_filter = (
+      "name",
+   )
+
+   ordering = (
+      "name",
+   )
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+   list_display =(
+      "id",
+      "name",
+      "phone",
+   )
+
+   search_fields =(
+      "name",
+      "phone",
+   )
+
+   list_filter = (
+      "name",
+   )
+
+   ordering = (
+      "name",
+   )
+
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+   list_display = (
+      "id",
+      "doctor",
+      "patient",
+      "appointment_date",
+      "status",
+   )
+
+   list_filter = (
+      "status",
+      "doctor",
+      "appointment_date",
+   )
+
+   search_fields = (
+      "doctor_name",
+      "patient_name",
+   )
+
+   ordering = (
+      "-appointment_date",
+   )
+
+   autocomplete_fields = (
+      "doctor",
+      "patient",
+   )
+
+   list_per_page = 20
+
+
--- Database_relationships/hospital/models.py
+++ Database_relationships/hospital/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+
+   name = models.CharField(max_length=100)
+   specialization = models.CharField(max_length=100)
+
+   def __str__(self):
+      return self.name 
+
+
+
+class Patient(models.Model):
+
+   name = models.CharField(max_length=100)
+   phone = models.CharField(max_length=20)
+
+   def __str__(self):
+      return self.name 
+
+
+
+class Appointment(models.Model):
+
+   class Status(models.TextChoices):
+      PENDING = "Pending", "Pending"
+      COMPLETED = "Completed", "Completed"
+      CANCELLED = "Cancelled", "Cancelled"
+
+   doctor = models.ForeignKey(
+      Doctor,
+      on_delete=models.CASCADE,
+      related_name="appointments",
+   )
+
+   patient = models.ForeignKey(
+      Patient,
+      on_delete=models.CASCADE,
+      related_name="appointments",
+   )
+
+   appointment_date = models.DateTimeField()
+
+   status = models.CharField(
+      max_length=20,
+      choices=Status.choices,
+      default=Status.PENDING
+   )
+
+   def __str__(self):
+      return (
+         f"{self.patient.name} | "
+         f"{self.doctor.name} | "
+         f"{self.appointment_date:%d-%m-%Y %I:%M %P}"
+      )
+
+   
+
+
+
+
+
