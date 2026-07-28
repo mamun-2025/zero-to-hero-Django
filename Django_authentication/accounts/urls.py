eb8a1b687544abf3441549d3bcbc01f09b98a4c7
Mamun Bepari <beparimamun708@gmail.com>
2026-07-28 16:12:57 +0600

    Add login, logout, dashboard

--- Django_authentication/accounts/urls.py
+++ Django_authentication/accounts/urls.py
@@ -7,5 +7,8 @@ app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
+    path("dashboard/", views.dashboard, name="dashboard"),
     
 ]
--- Django_authentication/accounts/views.py
+++ Django_authentication/accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
 
 def register(request):
 
@@ -27,4 +31,76 @@ def register(request):
       {
          "form":form,
       },
+   )
+
+
+
+def user_login(request):
+
+   form = AuthenticationForm(
+      request,
+      data=request.POST or None
+   )
+
+   if request.method == "POST":
+
+      if form.is_valid():
+
+         username = form.cleaned_data["username"]
+         password = form.cleaned_data["password"]
+
+         user = authenticate(
+            request,
+            username=username,
+            password=password,
+         )
+
+         if user is not None:
+
+            login(request, user)
+
+            return redirect("home")
+
+      return render(
+         request,
+         "accounts/login.html",
+         {
+            "form": form,
+         },
+      )
+
+# AuthenticationForm নিজেই Valid User Return করতে পারে।
+
+# def user_login(request):
+
+#     form = AuthenticationForm(request, data=request.POST or None)
+
+#     if request.method == "POST":
+
+#         if form.is_valid():
+
+#             login(request, form.get_user())
+
+#             return redirect("home")
+
+#     return render(
+#         request, "accounts/login.html", {"form": form}
+#     )
+
+# এটি Django-তে বহুল ব্যবহৃত এবং সংক্ষিপ্ত।
+
+
+def user_logout(request):
+
+   logout(request)
+
+   return redirect("Login")
+
+
+@login_required
+def dashboard(request):
+
+   return render(
+      request,
+      "dashboard.html",
    )
