3fa25fe765a9a36cec7374c4316eee21f450d040
Mamun Bepari <beparimamun708@gmail.com>
2026-07-31 07:58:45 +0600

    Fixed settings

--- Django_authentication/config/settings.py
+++ Django_authentication/config/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7h)d+lgi6v+@dvsr75s7%rn&j=h1p2mdl1j643g3am*%-(+epi'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -111,10 +112,16 @@ STATIC_URL = 'static/'
 
 AUTO_LOGOUT_DELAY = 15 * 60
 
-
+# # Terminal Email Backend
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 DEFAULT_FROM_EMAIL = "noreply@example.com"
 
 
 
+# EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST= "smtp.gmail.com"
+# EMAIL_PORT= 587
+# EMAIL_USER_TLS= True
+# EMAIL_HOST_USER= config("EMAIL_HOST_USER")
+# EMIAL_HOST_PASSWORD= config("EMAIL_HOST_PASSWORD")
+# DEFAULT_FROM_EMAIL= EMAIL_HOST_USER
